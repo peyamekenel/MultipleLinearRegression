62dbb87a21446712208c29c88f522acaadba9217
Peyam Ekenel <peyamekenel@hotmail.com>
2024-03-07 17:32:23 +0300

    first commit

---
+++ MultipleLinearRegression.R
@@ -0,0 +1,18 @@
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+dataset$State = factor(dataset$State,
+                         levels = c('New York', 'California', 'Florida'),
+                         labels = c(1, 2, 3))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State, data = training_set)
+y_pred = predict(regressor,newdata = test_set)
