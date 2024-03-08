59cff69f107b4a3919a0c69ed6e034b7ec7c2a16
Peyam Ekenel <peyamekenel@hotmail.com>
2024-03-08 11:22:16 +0300

    Backward Elimination Added

--- MultipleLinearRegression.R
+++ MultipleLinearRegression.R
@@ -1,11 +1,9 @@
-
 # Importing the dataset
 dataset = read.csv('50_Startups.csv')
 
 dataset$State = factor(dataset$State,
                          levels = c('New York', 'California', 'Florida'),
                          labels = c(1, 2, 3))
-
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
@@ -14,5 +12,25 @@ split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State, data = training_set)
+regressor = lm(formula = Profit ~ ., data = training_set)
+summary(regressor)
 y_pred = predict(regressor,newdata = test_set)
+
+#Backward Elimination(0.05 Signif. Level)
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
